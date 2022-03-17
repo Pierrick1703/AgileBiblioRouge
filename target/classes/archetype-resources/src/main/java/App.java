441fd91274d1470d67558d2691241ac6829d5c22
Pierrick COTTIN <cottin.pierrick@gmail.com>
2022-03-17 17:04:45 +0100

    first commit

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $esieeit_bibliotheque.AgileBiblioRouge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
